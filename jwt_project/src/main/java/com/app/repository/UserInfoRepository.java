e7a17f400ea8dd1e135e2a17ff5212969ee4bcd0
Satish <85325253+kalyankarsatish1234@users.noreply.github.com>
2024-09-03 18:50:22 +0530

    Add files via upload

---
+++ jwt_project/src/main/java/com/app/repository/UserInfoRepository.java
@@ -0,0 +1,13 @@
+package com.app.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.app.entity.UserInfo;
+
+@Repository
+public interface UserInfoRepository extends JpaRepository<UserInfo, Integer> {
+    Optional<UserInfo> findByEmail(String email); // Use 'email' if that is the correct field for login
+}
